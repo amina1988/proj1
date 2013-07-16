7f322fcd370e92aa85c03777c4589d154ce9b0a2
amina1988 <aminaelleuch@gmail.com>
2013-07-16 13:55:57 +0200

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Proj1::Application.routes.draw do
+  resources :users
+
 match '/help', to: 'static_pages#help', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
